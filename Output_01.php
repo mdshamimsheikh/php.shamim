43fd3cd9c6fd009b43ea0b4c7ab748a521d7358c
mdshamimsheikh <76837172+mdshamimsheikh@users.noreply.github.com>
2023-03-04 21:24:01 +0600

    Update and rename Output.php to Output_01.php

---
+++ Output_01.php
@@ -0,0 +1,37 @@
+<?php 
+
+
+echo 'Hello aim Shamim';
+echo '<br>';
+echo 12,'Shamim',56;
+
+print('<br>Md Shamim Sheikh');
+
+echo '<br>';
+print_r('Hello'); echo '<br>';
+
+
+print_r ([1,3,5,67,5]);
+echo '<br>';
+
+var_dump('Hello');
+
+var_export('Hello PHP');
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php</title>
+</head>
+<body>
+    <h1><?php 
+    echo 'Hello Post';
+    
+    ?></h1>
+</body>
+</html>
