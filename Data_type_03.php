67088b127b2aa63dd4edb9131b0af65a63d34212
mdshamimsheikh <76837172+mdshamimsheikh@users.noreply.github.com>
2023-03-04 21:13:50 +0600

    Add files via upload

---
+++ Data_type_03.php
@@ -0,0 +1,30 @@
+<?php
+
+
+//Variable
+
+$name='Md Shamim Sheikh';  //String
+$age=23;                   //Integer
+$cash=true;                //Boolean
+$mony=28.98;               //Float
+
+
+// echo 'My name is'.$name.'and age is '.$age;
+
+echo "My name is ${name} and my is ${age}";
+
+//Constant variable
+
+define('host','Shamim');
+define('cash',85000.10);
+
+echo cash;
+
+
+echo 4+6;   echo  '<br>';
+echo 4-6;echo  '<br>';
+echo 4*6;echo  '<br>';
+echo 4/6;echo  '<br>';
+echo 4%6;
+
+?>
