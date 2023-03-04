ec51f204c79543df2705d4379cf67f8125f9aa51
mdshamimsheikh <76837172+mdshamimsheikh@users.noreply.github.com>
2023-03-04 19:38:32 +0600

    Add files via upload

---
+++ Function.php
@@ -0,0 +1,53 @@
+<?php
+
+//PHP Function 
+
+// function register($email)
+// {
+//     echo $email.'<br>is registered';
+// }
+
+// register('mdshamimsheikh553@gmail.com');
+
+
+
+// function sum($x,$y)
+// {
+//     return $x+$y;
+// }
+
+// $number=sum(89,5);
+// echo $number;
+
+// $x=100;
+// $t=700;
+// function demo()
+// {
+//     global $x;
+//     global $t;
+//     $y=200;
+//     echo $x;
+//     echo '<br>'.$y;
+//     echo '<br>'.$t;
+// };
+
+// echo demo();
+
+
+// $add=function($a,$b)
+// {
+//    return $a+$b;
+// };
+
+// echo $add(10,90);
+
+
+$multiply=fn($a,$b)=>$a+$b;
+
+echo $multiply(10,80);
+
+
+
+
+
+?>
